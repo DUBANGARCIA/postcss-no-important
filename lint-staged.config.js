43b02b109b9e28f7ef9aa3afd1efb2bc9811ee32
Duban Andres Garcia Rodriguez <duban162008@gmail.com>
2025-08-08 08:04:56 -0500

    ci(workspace): 🎡 change the CI/CD token to the one provided by GitHub

--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  '*': [
-    'bun --bun biome format --write --no-errors-on-unmatched --files-ignore-unknown=true',
-    'bun --bun biome lint --write --unsafe --no-errors-on-unmatched --files-ignore-unknown=true',
-  ],
-  'package.json': ['bun --bun sort-package-json'],
-};
